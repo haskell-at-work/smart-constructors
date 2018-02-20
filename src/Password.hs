f0c9992af5c2a39323c22a91183b5dff637518b2
Oskar Wickström <oskar.wickstrom@gmail.com>
2018-02-20 08:59:33 +0100

    Implement password module

--- src/Password.hs
+++ src/Password.hs
@@ -1,2 +1,24 @@
-module Password where
+module Password
+  ( Password
+  , ValidationError(..)
+  , validatePassword
+  ) where
 
+import           Data.Text (Text)
+import qualified Data.Text as Text
+
+newtype Password =
+  Password Text
+
+data ValidationError
+  = TooShort
+  | TooLong
+  deriving (Eq, Show)
+
+validatePassword :: Text -> Either ValidationError Password
+validatePassword t
+  | l < 10 = Left TooShort
+  | l > 50 = Left TooLong
+  | otherwise = Right (Password t)
+  where
+    l = Text.length t
